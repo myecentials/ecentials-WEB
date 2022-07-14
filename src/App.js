c9e70187f941195c07d1b39adcb9463602fe843e
aopoku255 <aopoku255@gmail.com>
2022-07-14 19:00:35 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Config  from './config/config';
+import "bootstrap/dist/css/bootstrap.css"
+import "./assets/styles/global.css"
+function App() {
+  return (
+    <div className="App">
+      <Config />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/config.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Home from '../pages/Home'
+import Error from '../pages/Error'
+
+const Config = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='signup' element={<Signup/>}/>
+            <Route path='login' element={<Login/>}/>
+            <Route path='home' element={<Home/>}/>
+            <Route path='*' element={<Error/>}/>
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Config
