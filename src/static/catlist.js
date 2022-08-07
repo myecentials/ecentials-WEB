7446b775ec0d4e1b31a1698db43aa228338bab3b
Profand <aopoku255@gmail.com>
2022-08-07 11:30:00 +0000

    to be continued

---
+++ src/static/catlist.js
@@ -0,0 +1,29 @@
+let category = [
+  {
+    index: 1,
+    categoryName: "Medicine",
+    status: "Active",
+  },
+  {
+    index: 2,
+    categoryName: "Medicine",
+    status: "active",
+  },
+  {
+    index: 3,
+    categoryName: "Medicine",
+    status: "active",
+  },
+  {
+    index: 4,
+    categoryName: "Medicine",
+    status: "Inactive",
+  },
+  {
+    index: 5,
+    categoryName: "Medicine",
+    status: "active",
+  },
+];
+
+export default category;
