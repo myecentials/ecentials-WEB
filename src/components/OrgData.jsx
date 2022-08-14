d99b89d84c91294fd464df21a361313a482300ad
Profand <aopoku255@gmail.com>
2022-08-14 19:38:02 +0000

    added Bill Of Loading

--- src/components/OrgData.jsx
+++ src/components/OrgData.jsx
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect, useRef, useEffect, useState } from "react";
 import { OrgChart } from "d3-org-chart";
 import StaffCard from "./StaffCard";
+import morevert from "../assets/icons/svg/morevert.svg";
 import * as d3 from "d3";
 export const OrgChartComponent = (props, ref) => {
   const d3Container = useRef(null);
@@ -21,21 +22,22 @@ export const OrgChartComponent = (props, ref) => {
       chart
         .container(d3Container.current)
         .data(props.data)
-        .nodeHeight((d) => 85)
+        .nodeHeight((d) => 100)
         .nodeWidth((d) => {
           return 220;
         })
-        .childrenMargin((d) => 50)
-        .compactMarginBetween((d) => 25)
+        .childrenMargin((d) => 70)
+        .compactMarginBetween((d) => 80)
         .compactMarginPair((d) => 50)
         .neightbourMargin((a, b) => 25)
         .siblingsMargin((d) => 25)
+
         .initialZoom(0.7)
         .buttonContent(({ node, state }) => {
           return `<div style="px;color:#716E7B;border-radius:5px;padding:4px;font-size:10px;margin:auto auto;background-color:white;border: 1px solid #E4E2E9"> <span style="font-size:9px">${
             node.children
-              ? `<i class="fas fa-angle-up"></i>`
-              : `<i class="fas fa-angle-down"></i>`
+              ? `<i class="fa fa-angle-up"></i>`
+              : `<i class="fa fa-angle-down"></i>`
           }</span> ${node.data._directSubordinates}  </div>`;
         })
         .linkUpdate(function(d, i, arr) {
@@ -55,17 +57,17 @@ export const OrgChartComponent = (props, ref) => {
           const color = "#FFFFFF";
           return `
             <div style="background-color:${color}; position:absolute;margin-top:-1px; margin-left:-1px;width:${d.width}px;height:${d.height}px;border-radius:10px;border: none">
-               <div style="background-color:${color};position:absolute;margin-top:-25px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
+               <div style="background-color:${color};position:absolute;margin-top:-25px;margin-left:${20}px;border-radius:100px;width:50px;height:50px;" ></div>
                <img src=" ${
                  d.data.imageUrl
-               }" style="position:absolute;margin-top:-20px;margin-left:${20}px;border-radius:100px;width:40px;height:40px;" />
+               }" style="position:absolute;margin-top:-25px;margin-left:${20}px;border-radius:100px;width:50px;height:50px;" />
                
-              <div style="color:#08011E;position:absolute;right:20px;top:17px;font-size:10px;"><i class="fas fa-ellipsis-h"></i></div>
+              <div style="color:#A098AE;position:absolute;right:20px;top:17px;font-size:10px;"><i class="fa fa-ellipsis-h"></i></div>
 
-              <div style="font-size:15px;color:#08011E;margin-left:20px;margin-top:32px"> ${
+              <h6 style="font-size:18px;margin-left:20px;margin-top:32px; color: #303972"> ${
                 d.data.name
-              } </div>
-              <div style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
+              } </h6>
+              <div style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:16px;"> ${
                 d.data.positionName
               } </div>
 
