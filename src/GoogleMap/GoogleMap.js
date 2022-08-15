20d1d5eb9f69049493044e212d4b6a781f32987f
Profand <aopoku255@gmail.com>
2022-08-15 12:08:15 +0000

    added Bill Of Loading

--- src/GoogleMap/GoogleMap.js
+++ src/GoogleMap/GoogleMap.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
+import googleMapAPI from "../private/keys";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -14,13 +15,13 @@ export default function GoogleMapDisplay() {
 
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: "100%", width: "100%" }}>
+    <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{ key: googleMapAPI }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        {/* <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" /> */}
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
       </GoogleMapReact>
     </div>
   );
---
+++ src/private/keys.js
@@ -0,0 +1,3 @@
+const googleMapAPI = "AIzaSyBj4WJPBgp-ZDRPkCLNi5Afh61n6fVMQpQ";
+
+export default googleMapAPI;
