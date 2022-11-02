1087b28e9a0d433ce6cde01acd8c68f2487bdc99
unknown <aopoku255gmail.com>
2022-11-02 18:48:39 +0000

    Added Registration

--- src/private/keys.js
+++ src/private/keys.js
@@ -3,4 +3,4 @@ const googleMapAPI = "AIzaSyBj4WJPBgp-ZDRPkCLNi5Afh61n6fVMQpQ";
 export default googleMapAPI;
 
 export const BASE_URL =
-  " http://localhost:3001/api/v1/business-owner/create-business-owner";
+  "https://ecentials-e17c0.uc.r.appspot.com/api/v1/business-owner/create-business-owner";
