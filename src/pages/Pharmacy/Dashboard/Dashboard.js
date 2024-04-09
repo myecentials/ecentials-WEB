304df7b528509d0742bd09900ac0e83bc322bf30
Asheryram <ashertettehabotsi@gmail.com>
2024-04-09 20:54:33 +0000

    Added useEffect for updating key:pharmacyInfo

--- src/pages/Pharmacy/Dashboard/Dashboard.js
+++ src/pages/Pharmacy/Dashboard/Dashboard.js
@@ -1,49 +1,28 @@
 import React, { useEffect } from "react";
-// import { useState } from "react";
 import ItemsCard from "../../../components/Pharmacy/DAshboard/ItemsCard";
-// import SearchBar from "../../components/SearchBar";
-// import notification from "../../../assets/icons/svg/notification.svg";
-// import settings from "../../../assets/icons/svg/settings.svg";
-// import circleperson from "../../../assets/icons/svg/circleperson.svg";
-// import continueicon from "../../../assets/icons/svg/continue.svg";
 import { Link } from "react-router-dom";
-// import ActiveStaff from "../../components/ActiveStaff";
 import CurvedChat from "../../../components/Pharmacy/DAshboard/CurvedChat";
-// import SideBar from "../../../components/SideBar";
-// import { Collapse } from "reactstrap";
-// import DeliveryCalander from "../../components/DeliveryCalander";
-// import BarGraph from "../../components/BarGraph";
-// import Shipment from "../../components/Shipment";
 import { Helmet } from "react-helmet";
-// import NavBar from "../../../components/NavBar";
-// import dayofWeek from "../../../static/todaysDate";
-// import dayOfMonth from "../../../static/todaysDate";
-// import curMonth from "../../../static/todaysDate";
-// import curYear from "../../../static/todaysDate";
-// import todaysDate from "../../../static/todaysDate";
-// import deliverycard from "../../../assets/images/svgs/deliverycard.svg";
-// import hrmcard from "../../../assets/images/svgs/hrmcard.svg";
-// import returncard from "../../../assets/images/svgs/returncard.svg";
-// import NavIcons from "../../components/NavIcons";
-// import { BsSearch } from "react-icons/bs";
 import Search from "../../../components/Search";
-// import Header from "../../../components/Header";
 import PharmacyName from "../../../components/PharmacyName";
-// import axios from "../../config/api/axios";
 import product from "../../../assets/images/svgs/Finance.svg";
 import hrm from "../../../assets/images/svgs/hrm.svg";
 import invoice from "../../../assets/images/svgs/invoices.svg";
 import orders from "../../../assets/images/svgs/food.svg";
 import returns from "../../../assets/images/svgs/returns.svg";
 import sales from "../../../assets/images/svgs/sales.svg";
-// import customers from "../../assets/icons/svg/customer.svg";
 import DateHeader from "../../../components/DateHeader";
-// import HomeHeader from "../../components/HomeHeader";
 import { useDispatch, useSelector } from "react-redux";
 import { useGetMonthlySalesMutation } from "../../../app/features/dashboard/dashboardApiSlice";
-import { facility_id } from "../../../app/features/authSlice/authSlice";
+import { facility_id, pharmacyInfo } from "../../../app/features/authSlice/authSlice";
 import { monthlySales } from "../../../app/features/dashboard/dashboardSlice";
+import {
+  useGetSignupsQuery,
+} from "../../../app/features/authSlice/userApiSlice";
+import { useGetPharmacyInfoMutation } from "../../../app/features/authSlice/userApiSlice";
 
+
+import { Toaster,toast } from 'react-hot-toast';
 const Dashboard = () => {
   // const [isOpen, setIsOpen] = useState(false);
 
@@ -90,6 +69,46 @@ const Dashboard = () => {
   const dispatch = useDispatch();
   const [monthlysales] = useGetMonthlySalesMutation();
   const facilityid = useSelector(facility_id);
+  const { data: mydata } = useGetSignupsQuery();
+  const [getinfo] = useGetPharmacyInfoMutation();
+
+  useEffect(() => {
+   
+    const fetchData = async () => {
+      try{
+        const results = await getinfo(facilityid).unwrap();
+        dispatch(pharmacyInfo(results?.data));
+        sessionStorage.setItem("pharmacyInfo", JSON.stringify(results?.data));
+      }catch (error) {
+        console.log(error)
+        if (error.status === "FETCH_ERROR")
+				toast.error("Network Error ");
+      }
+      
+    };
+    fetchData();
+  }, [dispatch, facilityid, getinfo]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await mydata;
+
+        // setIsPharmacy(res.has_pharmacy);
+
+        // dispatch(setHasPharmacy({ ...res }));
+
+        sessionStorage.setItem("storeInfo", JSON.stringify(res));
+
+        // Process the response data here
+      } catch (error) {
+        // Handle any errors that occur during the request
+      }
+    };
+
+    fetchData();
+  }, [dispatch, mydata]);
+
   useEffect(() => {
     try{
       const fetchData = async () => {
@@ -116,6 +135,7 @@ const Dashboard = () => {
       <Helmet>
         <title>Dashboard</title>
       </Helmet>
+      <Toaster/>
         {/* MIDDLE */}
         <div className="col-md-9 px-3 middle">
        
