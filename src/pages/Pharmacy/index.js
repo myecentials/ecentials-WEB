0122a9515c6dd6d79a36f3921c5958d411ed91a5
Asheryram <ashertettehabotsi@gmail.com>
2024-04-11 15:10:24 +0000

    pharmInfo session storage update added

--- src/pages/Pharmacy/index.js
+++ src/pages/Pharmacy/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { useState } from "react";
+import { useEffect } from "react";
 // import ItemsCard from "../../components/ItemsCard";
 // import SearchBar from "../../components/SearchBar";
 // import notification from "../../assets/icons/svg/notification.svg";
@@ -44,7 +44,34 @@ import Header from "../../components/Header";
 // import { monthlySales } from "../../app/features/dashboard/dashboardSlice";
 import { Outlet} from "react-router-dom";
 // import Dashboard from "./Dashboard/Dashboard";
+import { facility_id, pharmacyInfo } from "../../app/features/authSlice/authSlice";
+import { useGetPharmacyInfoMutation } from "../../app/features/authSlice/userApiSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+
 const Pharmacy = () => {
+	const facilityid = useSelector(facility_id);
+	const [getinfo] = useGetPharmacyInfoMutation();
+	const dispatch = useDispatch();
+
+
+	useEffect(() => {
+   
+		const fetchData = async () => {
+		  try{
+			const results = await getinfo(facilityid).unwrap();
+			dispatch(pharmacyInfo(results?.data));
+			sessionStorage.setItem("pharmacyInfo", JSON.stringify(results?.data));
+		  }catch (error) {
+			console.log(error)
+			if (error.status === "FETCH_ERROR")
+					console.error("Network Error ");
+		  }
+		  
+		};
+		fetchData();
+	  }, [dispatch, facilityid, getinfo]);
+
 	return (
 		<>
 			<Header />
