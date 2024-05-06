d12655300d1994bdf2298d217d0f2d9d30b324f1
Asheryram <ashertettehabotsi@gmail.com>
2024-05-06 08:39:24 +0000

    Invoice is clean

---
+++ src/components/Pharmacy/Invoice/InvoiceNonDrugCard.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const InvoiceDrugCard = (props) => {
+
+  return (
+    <div
+      className="bg-white"
+      onClick={props.handleClick}
+      onFocus={props.handleFocus}
+      style={{borderRadius: "4px"}}
+    >
+      <input
+        type="checkbox"
+        name=""
+        id=""
+        className=" m-2"
+        onChange={props.handleChange}
+        disabled={props.stock <= 0}
+      />
+      <img
+        src={props.drug_img}
+        alt=""
+        width={100}
+        className="d-block mx-auto mt-2"
+        style={{
+          pointerEvents: "none",
+          aspectRatio: "3 / 2",
+          width: "75%",
+          objectFit: "contain",
+          mixBlendMode: "darken",
+        }}
+      />
+      <div className="d-flex justify-content-between align-items-center mx-2 mt-3">
+        <h6 className="" style={{ fontSize: "10px" }}>
+          {props.drug_name}
+        </h6>
+        <h6 style={{ fontSize: "10px", color: "#42CB91" }}>
+          GH₵{props.price}
+        </h6>
+      </div>
+
+      <div className="d-flex justify-content-between align-items-center mx-2 mt-3">
+        <p style={{ lineHeight: 0, fontSize: "11px" }} className="mx-2">
+          {props.category}
+        </p>
+        {/* <p style={{ lineHeight: 0, fontSize: "11px" ,borderRadius: "4px",  backgroundColor: "brown" }} className={`mx-2 text-white `}>
+            <div style={{ height: 10}}   className="p-2" >
+              Non-Drug
+            </div>
+        </p> */}
+      </div>
+      <h6 className="text-center mt-2" style={{ fontSize: "12px" }}>
+        ({props.stock}) Units
+      </h6>
+    </div>
+  );
+};
+
+export default InvoiceDrugCard;
